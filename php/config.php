95d6b5901b6b4ec6527d2a833a7df983db8dfc7f
Brendon Gonzalez <brendon@brandingbrand.com>
2014-03-17 13:35:45 -0400

    Config update.

--- php/config.php
+++ php/config.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 $base_url= 'http://www.anotherroundsailing.com/';
 //$base_url='http://localhost/dadio/';
-#remove the directory path we don't want 
-$request  = str_replace("/pages/", "", $_SERVER['REQUEST_URI']); 
- 
-#split the path by '/'  
-$params     = explode("/", $request); 
-#keeps users from requesting any file they want  
-$safe_pages = array();    
+#remove the directory path we don't want
+$request  = str_replace("/pages/", "", $_SERVER['REQUEST_URI']);
+
+#split the path by '/'
+$params     = explode("/", $request);
+#keeps users from requesting any file they want
+$safe_pages = array();
 
 //the email to receive your requests to
 $to_email="mike@anotherroundsailing.com";
@@ -17,7 +17,7 @@ $email_config = array ('host' => "mail.anotherroundsailing.com",
         //This needs to be a gmail address. Otherwise you have to update the host and port numbers.
         'username' => "mike@anotherroundsailing.com",
         //your GMAIL password. This just routes the email on your site through gmail's open smtp services.
-        'password' => "mikeg");
-		
+        'password' => "85Oday28");
+
 //1 for live, 2 for test - Probably won't need to change this. Email me if there is a problem. bmgonzalezmm@gmail.com
 $pkey=2;
